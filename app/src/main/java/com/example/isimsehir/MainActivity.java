4cdbe3dd69732baea8543575cadcd48b2e36e230
GokberkDokmen <gokberkdokmen@gmail.com>
2024-01-07 17:26:36 +0300

    testing

--- app/src/main/java/com/example/isimsehir/MainActivity.java
+++ app/src/main/java/com/example/isimsehir/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-                System.exit(0);
+                System.exit(0);//
             }
         });
     }
